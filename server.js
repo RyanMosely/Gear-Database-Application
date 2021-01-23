50b6c10ab8c27d4b47bfc728027e27f3330e39b5
Alek <alekvalencia2535@gmail.com>
2021-01-23 00:37:05 -0800

    installed firebase. changed require firestore to database since that is what we are using. Moved config file to routes since it would not work otherwise. commented a lot of code out on server.js. still not returning any information.

--- server.js
+++ server.js
@@ -16,36 +16,38 @@ app.use(express.json());
 
 // Firebase SDK
 // =============================================================
+// require("./server/database/firebaseConfig");
+require("./server/routes/dbroutes.js");
+// const firebase = require("firebase/app");
+// require("firebase/auth");
+// require("firebase/firestore");
 
-const firebase = require("firebase/app");
-require("firebase/auth");
-require("firebase/firestore");
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAhx1kN7KqwqIMH5Evj7ZfCrghDaQvdX8o",
+//   authDomain: "omegon-gda-default-rtdb.firebaseapp.com",
+//   databaseURL: "https://omegon-gda-default-rtdb.firebaseio.com",
+//   projectId: "omegon-gda-default-rtdb",
+//   storageBucket: "omegon-gda-default-rtdb.appspot.com",
+//   messagingSenderId: "827718843519",
+//   appId: "1:827718843519:web:07f06f36407e063135934a",
+//   // measurementId: "G-MEASUREMENT_ID",
+// };
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAhx1kN7KqwqIMH5Evj7ZfCrghDaQvdX8o",
-  authDomain: "omegon-gda-default-rtdb.firebaseapp.com",
-  databaseURL: "https://omegon-gda-default-rtdb.firebaseio.com",
-  projectId: "omegon-gda-default-rtdb",
-  storageBucket: "omegon-gda-default-rtdb.appspot.com",
-  messagingSenderId: "827718843519",
-  appId: "1:827718843519:web:07f06f36407e063135934a",
-  // measurementId: "G-MEASUREMENT_ID",
-};
-
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
 //------- Start routes
 // Routes
 // =============================================================
-
-app.use('./server/routes/dbroutes.js');
+// const firebase = require("./server/database/firebaseConfig")
+// app.use(firebase);
 // import routes to the app.js.
 // Routes
 //------- End routes
 
 app.get("*", (req, res) => {
-  const rootHtmlPath = path.resolve("./client/public", "index.html");
-  res.sendFile(rootHtmlPath);
+  // const rootHtmlPath = path.resolve("./client/public", "index.html");
+  // res.sendFile(rootHtmlPath);
+  res.sendFile(__dirname + "/client/public/index.html");
 });
 
 app.listen(port, () => {
--- server/routes/dbroutes.js
+++ server/routes/dbroutes.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const database = firebase.database();
+
+// Firebase SDK Config
+// ============================================================= sets up Firebase authorization
+
+const firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/database");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAhx1kN7KqwqIMH5Evj7ZfCrghDaQvdX8o",
+  authDomain: "omegon-gda-default-rtdb.firebaseapp.com",
+  databaseURL: "https://omegon-gda-default-rtdb.firebaseio.com",
+  projectId: "omegon-gda-default-rtdb",
+  storageBucket: "omegon-gda-default-rtdb.appspot.com",
+  messagingSenderId: "827718843519",
+  appId: "1:827718843519:web:07f06f36407e063135934a",
+  // measurementId: "G-MEASUREMENT_ID",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.database();
 
 router.get('/api/users', (req, res) => {
-    const data = database.ref('/users/').once('value').then((snapshot) => {
-        const username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+    try {
+      const data = db.ref('/users').on('value', snapshot => {
+        return snapshot.val();
       });
-    res.send(data);
+    res.status(200).send(data);
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({fail: "No Data to Read"})
+  }
   });
 
   module.exports = router;
