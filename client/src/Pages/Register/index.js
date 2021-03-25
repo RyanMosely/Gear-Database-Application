edd1a7b25afbe0488626375579bba6f30f59bb0b
Zach Corpuz <zachcorpuz@yahoo.com>
2021-03-24 22:03:36 -0700

    set the value for each form input and created onChange function to create values for our hooks to be pushed into the database

--- client/src/Pages/Register/index.js
+++ client/src/Pages/Register/index.js
@@ -11,45 +11,47 @@ const api = axios.create({
   baseURL: 'http://localhost:3001/'
 })
 
-function Register() {
-const [firstName, setFirstName] = useState("");
-const [lastName, setLastname] = useState("");
-const [email, setEmail] = useState("");
-const [phoneNumber, setPhoneNumber] = useState("");
-const [addressLine1, setAddressLine1] = useState("");
-const [addressLine2, setAddressLine2] = useState("");
-const [city, setCity] = useState("");
-const [country, setCountry] = useState("");
-const [state, setState] = useState("");
-const [zipCode, setZipCode] = useState("");
-const [occupation, setOccupation] = useState("");
-const registerUser = async () => {
-  let res = await api.post('/register', {
-    firstName: firstName,
-    lastName: lastName,
-    email: email,
-    phoneNumber: phoneNumber,
-    addressLine1: addressLine1,
-    addressLine2: addressLine2,
-    city: city,
-    country: country,
-    state: state,
-    zipCode: zipCode,
-    occupation: occupation
-  })
-  console.log(res);
-}
+const Register = () => {
 
-const login = () => {
-  Axios({
-    method: "POST",
-    data: {
-      first_name: FirstName
-    },
-    withCredentials: true,
-    url: "http://localhost:3001/login",
-  }).then((res) => console.log(res));
-}
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [addressLine1, setAddressLine1] = useState("");
+  const [addressLine2, setAddressLine2] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [occupation, setOccupation] = useState("");
+
+  const registerUser = async () => {
+    let res = await api.post('/register', {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      phoneNumber: phoneNumber,
+      addressLine1: addressLine1,
+      addressLine2: addressLine2,
+      city: city,
+      country: country,
+      state: state,
+      zipCode: zipCode,
+      occupation: occupation
+    })
+    console.log(res);
+  }
+
+  const login = () => {
+    Axios({
+      method: "POST",
+      data: {
+        first_name: FirstName
+      },
+      withCredentials: true,
+      url: "http://localhost:3001/login",
+    }).then((res) => console.log(res));
+  }
 
   return (
     <Container id="profile">
@@ -59,37 +61,37 @@ const login = () => {
             <Col>
               <Form.Group controlId="formFirstName">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter First Name" />
+                <Form.Control type="text" value={firstName} onChange={e => setFirstName(e.target.value)}  placeholder="Enter First Name" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="formLastName">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter Last Name" />
+                <Form.Control type="text" value={lastName} onChange={e => setLastName(e.target.value)} placeholder="Enter Last Name" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Enter email" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="formPhoneNumber">
                 <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="text" placeholder="Enter Phone Number" />
+                <Form.Control type="text" value={phoneNumber} onChange={e => setPhoneNumber(e.target.value)} placeholder="Enter Phone Number" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="AddressLineOne">
                 <Form.Label>Address Line 1</Form.Label>
-                <Form.Control type="text" placeholder="Enter Address" />
+                <Form.Control type="text" value={addressLine1} onChange={e => setAddressLine1(e.target.value)} placeholder="Enter Address" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="AddressLineTwo">
                 <Form.Label>Address Line 2</Form.Label>
-                <Form.Control type="text" placeholder="Enter Address 2 (Apartment, suite, etc.)" />
+                <Form.Control type="text" value={addressLine2} onChange={e => setAddressLine2(e.target.value)} placeholder="Enter Address 2 (Apartment, suite, etc.)" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
@@ -97,31 +99,31 @@ const login = () => {
               <Col>
               <Form.Group controlId="City">
                 <Form.Label>City</Form.Label>
-                <Form.Control type="text" placeholder="Enter City" />
+                <Form.Control type="text" value={city} onChange={e => setCity(e.target.value)} placeholder="Enter City" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="Country">
                 <Form.Label>Country</Form.Label>
-                <Form.Control type="text" placeholder="Enter Country" />
+                <Form.Control type="text" value={country} onChange={e => setCountry(e.target.value)} placeholder="Enter Country" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="State">
                 <Form.Label>State</Form.Label>
-                <Form.Control type="text" placeholder="Enter State" />
+                <Form.Control type="text" value={state} onChange={e => setState(e.target.value)} placeholder="Enter State" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="ZipCode">
                 <Form.Label>Zip Code</Form.Label>
-                <Form.Control type="text" placeholder="Enter Zip Code" />
+                <Form.Control type="text" value={zipCode} onChange={e => setZipCode(e.target.value)} placeholder="Enter Zip Code" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
               <Form.Group controlId="Occupation">
                 <Form.Label>Occupation</Form.Label>
-                <Form.Control type="text" placeholder="Enter Occupation" />
+                <Form.Control type="text" value={occupation} onChange={e => setOccupation(e.target.value)} placeholder="Enter Occupation" />
                   <Form.Text className="text-muted">
                   </Form.Text>
               </Form.Group>
